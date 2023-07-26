d783d6df3ef5176a1b242aaaa7c653107e35d023
modaresimr <modaresi.mr@gmail.com>
2023-07-26 22:23:11 +0000

    update

--- train_and_test/ali/ali_main.sh
+++ train_and_test/ali/ali_main.sh
@@ -29,3 +29,19 @@ python3 ali_runner.py ali_configs/segpc_2class/segpc2021_2c_unetpp.yaml
 python3 ali_runner.py ali_configs/segpc_2class/segpc2021_2c_missformer.yaml
 python3 ali_runner.py ali_configs/segpc_2class/segpc2021_2c_transunet.yaml
 python3 ali_runner.py ali_configs/segpc_2class/segpc2021_2c_multiresunet.yaml
+
+
+
+
+
+
+
+python3 ali_runner.py ali_configs/segpc/segpc2021_adaptuctransnet_7.yaml
+python3 ali_runner.py ./ali_configs/segpc/segpc2021_unet.yaml
+python3 ali_runner.py ./ali_configs/segpc/segpc2021_uctransnet.yaml
+python3 ali_runner.py ./ali_configs/segpc/segpc2021_unetpp.yaml
+python3 ali_runner.py ./ali_configs/segpc/segpc2021_transunet.yaml
+python3 ali_runner.py ./ali_configs/segpc/segpc2021_resunet.yaml
+python3 ali_runner.py ./ali_configs/segpc/segpc2021_multiresunet.yaml
+python3 ali_runner.py ./ali_configs/segpc/segpc2021_missformer.yaml
+python3 ali_runner.py ./ali_configs/segpc/segpc2021_attunet.yaml
